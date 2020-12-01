f718a985b4af71ab0864a15cb5dcf94f54f13c17
horvathbenett9 <75091388+horvathbenett9@users.noreply.github.com>
2020-12-01 18:47:15 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,15 @@
 let x = 0
 let y = 0
 input.onButtonPressed(Button.A, function () {
-    basic.clearScreen()
-    for (let index = 0; index <= 24; index++) {
-        x = randint(0, 4)
-        y = randint(0, 4)
-        led.plot(x, y)
-        basic.pause(300)
+    for (let index = 0; index < 4; index++) {
+        basic.clearScreen()
+        for (let index_2 = 0; index_2 <= 32; index_2++) {
+            x = randint(0, 4)
+            y = randint(0, 4)
+            led.toggle(x, y)
+            basic.pause(300)
+        }
+        basic.pause(9000)
     }
 })
 basic.forever(function () {
